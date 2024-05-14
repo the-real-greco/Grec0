0a62c4576b68c0deee6a83dee0b14f916c7a74c1
the-real-greco <45739657+the-real-greco@users.noreply.github.com>
2024-05-14 09:10:59 -0700

    Update uci.h

--- include/uci.h
+++ include/uci.h
@@ -2,10 +2,13 @@
 #define UCI_H
 
 #include <string>
+#include "board.h"
 
 namespace UCI {
     void loop();
     void setPosition(const std::string& positionCommand);
+
+    extern Board board;  // Declare the board object
 }
 
 #endif // UCI_H
